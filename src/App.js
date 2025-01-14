9e41c4600c9b92b30aa3be4e3f2fbbdcf709d8f9
Zelin P Peter <zelin.pu@kirain.com>
2025-01-14 10:30:11 +0800

    init and update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import { useState } from 'react'
+import { useState, Suspense, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, TransformControls, useCursor } from '@react-three/drei'
+import { OrbitControls, TransformControls, useCursor, useFBX } from '@react-three/drei'
 import { useControls } from 'leva'
-import create from 'zustand'
+import * as THREE from 'three';
+// import {useStore} from 'zustand'
 
-const useStore = create((set) => ({ target: null, setTarget: (target) => set({ target }) }))
+
 
 function Box(props) {
-  const setTarget = useStore((state) => state.setTarget)
+  // const setTarget = useStore((state) => state.setTarget)
   const [hovered, setHovered] = useState(false)
   useCursor(hovered)
   return (
-    <mesh {...props} onClick={(e) => setTarget(e.object)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}>
+    <mesh {...props} onClick={()=>{}} onPointerOut={() => setHovered(false)}>
       <boxGeometry />
       <meshNormalMaterial />
     </mesh>
   )
 }
 
+function AllFBX() {
+  let fbx = useFBX('/all.fbx')
+  useEffect(() => {
+    // Traverse the FBX object to find bones
+    const bones = [];
+    fbx.traverse((object) => {
+      if (object instanceof THREE.Bone) {
+        bones.push(object);
+      }
+    });
+
+    // Log the bones to the console
+    console.log('Bones in the FBX model:', bones);
+  }, [fbx]);
+  return <primitive object={fbx} />
+}
+
 export default function App() {
-  const { target, setTarget } = useStore()
+  // const { target, setTarget } = useStore()
   const { mode } = useControls({ mode: { value: 'translate', options: ['translate', 'rotate', 'scale'] } })
   return (
-    <Canvas dpr={[1, 2]} onPointerMissed={() => setTarget(null)}>
+    <Canvas dpr={[1, 2]}>
+      <Suspense fallback={null}>
+        <AllFBX position={[2, 2, 0]} />
+      </Suspense>
       <Box position={[2, 2, 0]} />
       <Box />
-      {target && <TransformControls object={target} mode={mode} />}
+      {/* {target && <TransformControls mode={mode} />} */}
       <OrbitControls makeDefault />
     </Canvas>
   )
